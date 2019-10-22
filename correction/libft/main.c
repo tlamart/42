b1fdb529e70fffd8a6cfef3dd70e270e45feee38
tlamart <tlamart@student.42.fr>
2019-10-22 07:45:21 +0200

    add libft

---
+++ correction/libft/main.c
@@ -0,0 +1,81 @@
+#include "libft.h"
+#include <stdlib.h>
+
+int		main(void)
+{
+	ft_memset();
+	memset();
+	// check memset
+	
+	ft_bzero();
+	bzero();
+	// check bzero
+	
+	ft_memcpy();
+	memcpy();
+	// check memcpy
+	
+	ft_memccpy();
+	memccpy();
+	// check memcpy
+	
+	ft_memmove();
+	memmove();
+	// check memmove
+	
+	ft_memchr(); memchr();
+	// check memchr
+	
+	ft_memcpm(); memcmp();
+	// check memcmp
+	
+	ft_strlen(); strlen();
+	// check strlen
+	
+	ft_isalpha(); isalpha();
+	// check isalpha
+	
+	ft_isdigit(); isdigit();
+	// check isdigit;
+	
+	ft_isalnum(); isalnum();
+	// check isalnum
+	
+	ft_isascii(); isascii();
+	// check isascii
+	
+	ft_isprint(); isprint();
+	// check isprint
+	
+	ft_toupper(); toupper();
+	// check toupper
+	
+	ft_tolower(); tolower();
+	// check tolower
+	
+	ft_strchr(); strchr();
+	// check strchr
+	
+	ft_strrchr(); strrchr();
+	// check strrchr
+	
+	ft_strncmp(); strncmp();
+	// check strncmp
+	
+	ft_strlcpy(); strlcpy();
+	// check strlcpy
+	
+	ft_strlcat(); strlcat();
+	// check strlcat
+	
+	ft_strnstr(); strnstr();
+	// check strnstr
+	
+	ft_atoi(); atoi();
+	// check atoi
+
+	ft_calloc(); calloc();
+	// check calloc
+	
+	ft_strdup(); strdup();
+	// check strdup
